29be41ff241d677c15c0ec070a48039a2b5c00c4
Lucas D <l.dupias@net.estia.fr>
2019-05-09 15:02:19 +0200

    added thunk

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk';
 import rootReducer from './store/reducer/rootReducer'
 
-const store = createStore(rootReducer)
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+);
+
+
+//without thunk
+//const store = createStore(rootReducer)
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
